edb4416a12dd0510614cf7fec6ed85985d234dd0
Juli132 <jlmugiwara@gmail.com>
2026-06-28 23:47:55 -0400

    fixed abstraction

--- src/main/java/maindeveloper/dialects/KlipperVisitor.java
+++ src/main/java/maindeveloper/dialects/KlipperVisitor.java
@@ -9,7 +9,6 @@ public class KlipperVisitor extends GCodeVisitor {
         super(profile);
     }
 
-    // ---- Klipper-specific
     @Override
     protected String emitMacroHeader(String macroName) {
         return "[gcode_macro " + macroName + "]\ngcode:\n";
@@ -44,22 +43,16 @@ public class KlipperVisitor extends GCodeVisitor {
 
     @Override
     protected String emitSetHeater(String target, double value) {
-        return emitHeat(target, value, false);
-    }
-
-    @Override
-    protected String emitPause() {
-        return "PAUSE\n";
-    }
-
-    @Override
-    protected String emitResume() {
-        return "RESUME\n";
-    }
-
-    @Override
-    protected String emitRespond(String message) {
-        return "RESPOND MSG=\"" + message + "\"\n";
+        switch (target) {
+            case "extruder":
+                return "M104 S" + (int) value + "\n";
+            case "bed":
+                return "M140 S" + (int) value + "\n";
+            case "chamber":
+                return "M141 S" + (int) value + "\n";
+            default:
+                return "";
+        }
     }
 
     @Override
@@ -81,8 +74,121 @@ public class KlipperVisitor extends GCodeVisitor {
     }
 
     @Override
+    protected String emitWaitForTemp(String target) {
+        switch (target) {
+            case "extruder":
+                return "M109\n";
+            case "bed":
+                return "M190\n";
+            case "chamber":
+                return "M141\n";
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    protected String emitHome(String coordList) {
+        if (coordList != null && !coordList.isEmpty()) {
+            return "G28 " + coordList + "\n";
+        }
+        return "G28\n";
+    }
+
+    @Override
+    protected String emitMove(String direction) {
+        switch (direction) {
+            case "left":
+                return "G1 X-1\n";
+            case "right":
+                return "G1 X1\n";
+            case "center":
+                return "G1 X0 Y0\n";
+            case "up":
+                return "G1 Z1\n";
+            case "down":
+                return "G1 Z-1\n";
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    protected String emitMoveTo(String coordList) {
+        if (coordList != null && !coordList.isEmpty()) {
+            return "G1 " + coordList + "\n";
+        }
+        return "";
+    }
+
+    @Override
+    protected String emitSetSpeed(double value) {
+        return "G1 F" + (int) value + "\n";
+    }
+
+    @Override
+    protected String emitSetFan(double value) {
+        return "SET_FAN SPEED=" + (int) value + "\n";
+    }
+
+    @Override
+    protected String emitAbsolute() {
+        return "G90\n";
+    }
+
+    @Override
+    protected String emitRelative() {
+        return "G91\n";
+    }
+
+    @Override
+    protected String emitRelativeExtrusion() {
+        return "M83\n";
+    }
+
+    @Override
+    protected String emitResetExtruder() {
+        return "G92 E0\n";
+    }
+
+    @Override
+    protected String emitPause() {
+        return "PAUSE\n";
+    }
+
+    @Override
+    protected String emitResume() {
+        return "RESUME\n";
+    }
+
+    @Override
+    protected String emitDwell(double milliseconds) {
+        return "G4 P" + (int) milliseconds + "\n";
+    }
+
+    @Override
     protected String emitTimeoutSet(double seconds) {
         return "SET_IDLE_TIMEOUT TIMEOUT=" + (int) seconds + "\n";
+    }
+
+    @Override
+    protected String emitRespond(String message) {
+        return "RESPOND MSG=\"" + message + "\"\n";
+    }
+
+    @Override
+    protected String emitPrintFile(String filename) {
+        return "SDCARD_PRINT_FILE FILENAME=\"" + filename + "\"\n";
+    }
+
+    @Override
+    protected String emitMacroCall(String macroName) {
+        return macroName + "\n";
+    }
+
+    @Override
+    protected String emitBedMeshCalibrate() {
+        return "BED_MESH_CALIBRATE\n";
     }
 
     @Override
@@ -101,23 +207,28 @@ public class KlipperVisitor extends GCodeVisitor {
     }
 
     @Override
-    protected String emitSetFan(double value) {
-        return "SET_FAN SPEED=" + (int) value + "\n";
+    protected String emitSetNozzle(double value) {
+        return "";
     }
 
     @Override
-    protected String emitMacroCall(String macroName) {
-        return macroName + "\n";
+    protected String emitSetFilament(double value) {
+        return "";
     }
 
     @Override
-    protected String emitBedMeshCalibrate() {
-        return "BED_MESH_CALIBRATE\n";
+    protected String emitSetLayerHeight(double value) {
+        return "";
     }
 
     @Override
-    protected String emitPrintFile(String filename) {
-        return "SDCARD_PRINT_FILE FILENAME=\"" + filename + "\"\n";
+    protected String emitSetExtrusionMultiplier(double value) {
+        return "";
+    }
+
+    @Override
+    protected String emitEnableAutoExtrude(boolean enabled) {
+        return "";
     }
 
     @Override
@@ -128,5 +239,16 @@ public class KlipperVisitor extends GCodeVisitor {
     @Override
     protected String emitIfEnd() {
         return "{% endif %}\n";
+    }
+
+    @Override
+    protected String emitLayerStart(int layer) {
+        double z = layer * settings.getLayerHeight();
+        return "G1 Z" + String.format("%.3f", z) + "\n";
+    }
+
+    @Override
+    protected String emitLayerEnd() {
+        return "";
     }
 }
